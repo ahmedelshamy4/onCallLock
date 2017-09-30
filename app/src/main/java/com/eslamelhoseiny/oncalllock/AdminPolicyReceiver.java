a5f4867454aed9700e6d4f1f6e81258b0c974938
ahmedelshamy4 <ahmdalshamy173@gmail.com>
2017-09-29 19:34:18 -0700

    uploade project

---
+++ app/src/main/java/com/eslamelhoseiny/oncalllock/AdminPolicyReceiver.java
@@ -0,0 +1,10 @@
+package com.eslamelhoseiny.oncalllock;
+
+import android.app.admin.DeviceAdminReceiver;
+
+/**
+ * Created by Eslam Elhoseiny on 9/15/2017.
+ */
+
+public class AdminPolicyReceiver extends DeviceAdminReceiver {
+}
